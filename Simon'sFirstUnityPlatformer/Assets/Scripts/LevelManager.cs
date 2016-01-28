f1e57e9ff078651cd024894201e1f7b6afc3d1f2
Simon <simon.hergenhan@baxter-academy.org>
2016-01-28 10:19:32 -0500

    SECOND

---
+++ Simon'sFirstUnityPlatformer/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public GameObject currentCheckpoint;
+
+	private PlayerController player;
+
+	public GameObject playerParticle;
+
+	public float respawnDelay;
+
+	private float gravityStore;
+
+	// Use this for initialization
+	void Start () {
+		player = FindObjectOfType<PlayerController> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void RespawnPlayer()
+	{
+		StartCoroutine ("RespawnPlayerCo");
+	}
+
+	public IEnumerator RespawnPlayerCo ()
+	{
+		Instantiate (playerParticle, player.transform.position, player.transform.rotation);
+		player.enabled = false;
+		player.GetComponent<Renderer> ().enabled = false;
+		gravityStore = player.GetComponent<Rigidbody2D> ().gravityScale;
+		player.GetComponent<Rigidbody2D> ().gravityScale = 0f;
+		player.GetComponent<Rigidbody2D> ().velocity = Vector2.zero;
+		Debug.Log ("Player Respawn");
+		yield return new WaitForSeconds (respawnDelay);
+		player.GetComponent<Rigidbody2D> ().gravityScale = gravityStore;
+		player.transform.position = currentCheckpoint.transform.position;
+		player.enabled = true;
+		player.GetComponent<Renderer> ().enabled = true;
+		Instantiate (playerParticle, currentCheckpoint.transform.position, currentCheckpoint.transform.rotation);
+	}
+}
--- Simon'sFirstUnityPlatformer/Assets/Scripts/PlayerController.cs
+++ Simon'sFirstUnityPlatformer/Assets/Scripts/PlayerController.cs
@@ -7,9 +7,13 @@ public class PlayerController : MonoBehaviour {
 	public float jumpHeight;
 
 	public Transform groundCheck;
+	public Transform wallCheck;
+	public Transform wallCheck2;
 	public float groundCheckRadius;
 	public LayerMask whatIsGround;
 	private bool grounded;
+	private bool walled;
+	private bool walled2;
 
 	private bool doubleJumped;
 
@@ -21,23 +25,25 @@ public class PlayerController : MonoBehaviour {
 	void FixedUpdate () {
 		
 		grounded = Physics2D.OverlapCircle (groundCheck.position, groundCheckRadius, whatIsGround);
+		walled = Physics2D.OverlapCircle (wallCheck.position, groundCheckRadius, whatIsGround);
+		walled2 = Physics2D.OverlapCircle (wallCheck2.position, groundCheckRadius, whatIsGround);
 
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 	
-		if (grounded) 
+		if (grounded || walled || walled2) 
 		{
 			doubleJumped = false;
 		}
 
-		if(Input.GetKeyDown (KeyCode.Space) && grounded) 
+		if(Input.GetKeyDown (KeyCode.Space) && (grounded || walled || walled2)) 
 		{
 			GetComponent<Rigidbody2D>().velocity = new Vector2 (GetComponent<Rigidbody2D>().velocity.x, jumpHeight);
 		}
 
-		if(Input.GetKeyDown (KeyCode.Space) && !doubleJumped && !grounded) 
+		if(Input.GetKeyDown (KeyCode.Space) && !doubleJumped && (!grounded || !walled || !walled2)) 
 		{
 			GetComponent<Rigidbody2D>().velocity = new Vector2 (GetComponent<Rigidbody2D>().velocity.x, jumpHeight);
 			doubleJumped = true;
